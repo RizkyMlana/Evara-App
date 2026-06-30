362ab363f98bb1addd56750fde505d3103656ade
RizkyMlana <rizkyzky345@gmail.com>
2026-07-01 00:40:28 +0700

    initialize supabase

---
+++ lib/features/auth/data/models/login_request.dart
@@ -0,0 +1,15 @@
+class LoginRequest {
+  final String email;
+  final String password;
+  const LoginRequest({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "email": email,
+      "password": password,
+    };
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:evara_app/core/constants/env.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Supabase.initialize(url: Env.supabaseUrl, publishableKey: Env.supabaseAnonKey);
   runApp(const MyApp());
 }
 
